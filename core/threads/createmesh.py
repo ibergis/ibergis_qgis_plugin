7121bc5fc04f94534c51ec12371b9fe854e497eb
Pablo Marques <pahbloo@gmail.com>
2023-09-08 11:19:52 -0300

    Create mesh from roof layer

--- core/threads/createmesh.py
+++ core/threads/createmesh.py
@@ -21,10 +21,27 @@ class GwCreateMeshTask(GwTask):
         super().run()
         try:
             self.dao = global_vars.gpkg_dao_data.clone()
+
             ground_layer = core.get_layer(self.dao, "ground")
             core.validate_layer(ground_layer)
             poly_ground_layer = triangulate_custom(ground_layer, feedback=self.feedback)
+
+            roof_layer = core.get_layer(self.dao, "roof")
+            core.validate_layer(roof_layer)
+            crs = roof_layer.crs()
+            features = (
+                core.feature_to_layer(feature, crs)
+                for feature in roof_layer.getFeatures()
+            )
+            roof_meshes = (
+                triangulate_custom(feature, feedback=self.feedback)
+                for feature in features
+            )
+            poly_roof_layer = core.join_layers(roof_meshes)
+
             QgsProject.instance().addMapLayer(poly_ground_layer)
+            QgsProject.instance().addMapLayer(poly_roof_layer)
+
             return True
         except Exception:
             self.exception = traceback.format_exc()
--- core/threads/createmesh_core.py
+++ core/threads/createmesh_core.py
@@ -1,10 +1,31 @@
-from qgis.core import QgsVectorLayer
+from qgis.core import QgsVectorLayer, QgsField
+
+
+def feature_to_layer(feature, crs):
+    layer = QgsVectorLayer("MultiPolygon", "temp", "memory")
+    layer.startEditing()
+    layer.setCrs(crs)
+    for field in feature.fields():
+        layer.addAttribute(QgsField(field))
+    layer.addFeature(feature)
+    layer.commitChanges()
+    layer.updateExtents()
+    return layer
 
 
 def get_layer(dao, layer_name):
     path = f"{dao.db_filepath}|layername={layer_name}"
     return QgsVectorLayer(path, layer_name, "ogr")
 
+
+def join_layers(iterable):
+    joined_layer = next(iterable).clone()
+    for layer in iterable:
+        joined_layer.startEditing()
+        joined_layer.addFeatures(layer.getFeatures())
+        joined_layer.commitChanges()
+    joined_layer.updateExtents()
+    return joined_layer
 
 def validate_layer(layer):
     if not layer.isValid():
