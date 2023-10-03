919d2d3150b86d75865d7a2dff85b413c6eafd97
Pablo Marques <pahbloo@gmail.com>
2023-10-03 10:18:44 -0300

    Inline some methods

--- core/toolbars/utilities/createmesh_btn.py
+++ core/toolbars/utilities/createmesh_btn.py
@@ -24,7 +24,15 @@ class GwCreateMeshButton(GwAction):
         self.dlg_mesh = GwCreateMeshUi()
         dlg = self.dlg_mesh
 
-        self._check_for_previous_results()
+        # Check for previous results
+        self.temp_meshes = {}
+        root = QgsProject.instance().layerTreeRoot()
+        temp_group = tools_qgis.find_toc_group(root, "Mesh Temp Layers")
+        if temp_group is not None:
+            for layer in temp_group.findLayers():
+                if layer.name() in ["Ground Mesh", "Roof Mesh"]:
+                    self.temp_meshes[layer.name()] = layer
+
         save_temp_meshes = False
         if self.temp_meshes:
             message = (
@@ -38,7 +46,45 @@ class GwCreateMeshButton(GwAction):
         else:
             self.ground_layer = self._get_layer(self.dao, "ground")
             self.roof_layer = self._get_layer(self.dao, "roof")
-            errors = self._validate_input_layers()
+
+            # Validate input layers
+            errors = {}
+            errors_ground_layer = []
+            if not self.ground_layer.isValid():
+                errors_ground_layer.append(
+                    "Ground layer is not valid. Check if GPKG file has a 'ground' layer."
+                )
+            else:
+                if not all(
+                    type(feature["cellsize"]) in [int, float]
+                    and feature["cellsize"] > 0
+                    for feature in self.ground_layer.getFeatures()
+                ):
+                    errors_ground_layer.append(
+                        "There are invalid values in the column 'cellsize' of ground layer. "
+                        "The values of 'cellsize' must be greater than zero."
+                    )
+            if errors_ground_layer:
+                errors["ground_layer"] = errors_ground_layer
+
+            errors_roof_layer = []
+            if not self.roof_layer.isValid():
+                errors_roof_layer.append(
+                    "Roof layer is not valid. Check if GPKG file has a 'roof' layer."
+                )
+            else:
+                if not all(
+                    type(feature["cellsize"]) in [int, float]
+                    and feature["cellsize"] > 0
+                    for feature in self.roof_layer.getFeatures()
+                ):
+                    errors_roof_layer.append(
+                        "There are invalid values in the column 'cellsize' of roof layer. "
+                        "The values of 'cellsize' must be greater than zero."
+                    )
+            if errors_roof_layer:
+                errors["roof_layer"] = errors_roof_layer
+
             if errors:
                 message = "**Please verify these problems in the input layers before proceeding:**\n\n"
                 if "ground_layer" in errors:
@@ -52,28 +98,25 @@ class GwCreateMeshButton(GwAction):
                 dlg.lbl_warnings.hide()
 
         tools_gw.load_settings(dlg)
-        self._load_user_values()
-        self._set_initial_signals()
         tools_gw.disable_tab_log(dlg)
 
-        tools_gw.open_dialog(dlg, dlg_name="create_mesh")
+        # Set initial signals
+        dlg = self.dlg_mesh
+        dlg.btn_ok.clicked.connect(self._execute_process)
+        dlg.btn_cancel.clicked.connect(dlg.reject)
+        dlg.rejected.connect(partial(tools_gw.close_dialog, dlg))
 
-    def _check_for_previous_results(self):
-        self.temp_meshes = {}
-        root = QgsProject.instance().layerTreeRoot()
-        temp_group = tools_qgis.find_toc_group(root, "Mesh Temp Layers")
-        if temp_group is None:
-            return
-        for layer in temp_group.findLayers():
-            if layer.name() in ["Ground Mesh", "Roof Mesh"]:
-                self.temp_meshes[layer.name()] = layer
+        tools_gw.open_dialog(dlg, dlg_name="create_mesh")
 
     def _execute_process(self):
         dlg = self.dlg_mesh
         self.feedback = Feedback()
-        if not self._validate_inputs():
+
+        # Validate inputs
+        if not dlg.chk_ground.isChecked() and not dlg.chk_roof.isChecked():
+            tools_qt.show_info_box("Select at least one mesh to be created.")
             return
-        self._save_user_values()
+
         self.thread = GwCreateMeshTask(
             "Import INP file",
             ground_layer=self.ground_layer if dlg.chk_ground.isChecked() else None,
@@ -104,9 +147,6 @@ class GwCreateMeshButton(GwAction):
         path = f"{dao.db_filepath}|layername={layer_name}"
         return QgsVectorLayer(path, layer_name, "ogr")
 
-    def _load_user_values(self):
-        self._user_values("load")
-
     def _on_task_completed(self):
         self._on_task_end("Task finished!")
 
@@ -135,15 +175,6 @@ class GwCreateMeshButton(GwAction):
     def _save_meshes(self):
         pass
 
-    def _save_user_values(self):
-        self._user_values("save")
-
-    def _set_initial_signals(self):
-        dlg = self.dlg_mesh
-        dlg.btn_ok.clicked.connect(self._execute_process)
-        dlg.btn_cancel.clicked.connect(dlg.reject)
-        dlg.rejected.connect(partial(tools_gw.close_dialog, dlg))
-
     def _set_progress_text(self, txt):
         tools_gw.fill_tab_log(
             self.dlg_mesh,
@@ -153,69 +184,3 @@ class GwCreateMeshButton(GwAction):
         )
         sb = self.dlg_mesh.txt_infolog.verticalScrollBar()
         sb.setValue(sb.maximum())
-
-    def _user_values(self, action):
-        txt_widgets = []
-
-        for widget in txt_widgets:
-            if action == "load":
-                value = tools_gw.get_config_parser(
-                    "create_mesh",
-                    widget,
-                    "user",
-                    "session",
-                )
-                tools_qt.set_widget_text(self.dlg_mesh, widget, value)
-            elif action == "save":
-                value = tools_qt.get_text(self.dlg_mesh, widget, False, False)
-                value = value.replace("%", "%%")
-                tools_gw.set_config_parser(
-                    "create_mesh",
-                    widget,
-                    value,
-                )
-
-    def _validate_input_layers(self):
-        errors = {}
-        errors_ground_layer = []
-        if not self.ground_layer.isValid():
-            errors_ground_layer.append(
-                "Ground layer is not valid. Check if GPKG file has a 'ground' layer."
-            )
-        else:
-            if not all(
-                type(feature["cellsize"]) in [int, float] and feature["cellsize"] > 0
-                for feature in self.ground_layer.getFeatures()
-            ):
-                errors_ground_layer.append(
-                    "There are invalid values in the column 'cellsize' of ground layer. "
-                    "The values of 'cellsize' must be greater than zero."
-                )
-        if errors_ground_layer:
-            errors["ground_layer"] = errors_ground_layer
-
-        errors_roof_layer = []
-        if not self.roof_layer.isValid():
-            errors_roof_layer.append(
-                "Roof layer is not valid. Check if GPKG file has a 'roof' layer."
-            )
-        else:
-            if not all(
-                type(feature["cellsize"]) in [int, float] and feature["cellsize"] > 0
-                for feature in self.roof_layer.getFeatures()
-            ):
-                errors_roof_layer.append(
-                    "There are invalid values in the column 'cellsize' of roof layer. "
-                    "The values of 'cellsize' must be greater than zero."
-                )
-        if errors_roof_layer:
-            errors["roof_layer"] = errors_roof_layer
-
-        return errors
-
-    def _validate_inputs(self):
-        dlg = self.dlg_mesh
-        if not dlg.chk_ground.isChecked() and not dlg.chk_roof.isChecked():
-            tools_qt.show_info_box("Select at least one mesh to be created.")
-            return False
-        return True
