f96b2db850f2cc395174c7a4b13dce24628b6fb2
Pablo Marques <pahbloo@gmail.com>
2023-10-11 15:26:02 -0300

    Remove unused functions

--- core/threads/createmesh_core.py
+++ core/threads/createmesh_core.py
@@ -38,21 +38,6 @@ def feature_to_layer(feature, crs):
     return layer
 
 
-def get_layer(dao, layer_name):
-    path = f"{dao.db_filepath}|layername={layer_name}"
-    return QgsVectorLayer(path, layer_name, "ogr")
-
-
-def join_layers(iterable):
-    joined_layer = next(iterable).clone()
-    for layer in iterable:
-        joined_layer.startEditing()
-        joined_layer.addFeatures(layer.getFeatures())
-        joined_layer.commitChanges()
-    joined_layer.updateExtents()
-    return joined_layer
-
-
 def validate_input_layers(layers_dict):
     error_layers = []
 
