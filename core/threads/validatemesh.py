02eef3fe205d35434c172db0b766a5c0c432441b
nullptr <eliesbertran@gmail.com>
2023-10-23 16:00:05 +0200

    Added roof attributres check

--- core/threads/validatemesh.py
+++ core/threads/validatemesh.py
@@ -8,7 +8,7 @@ import shapely
 import itertools
 
 
-def validate_cellsize(layer):
+def validate_cellsize(layer: QgsVectorLayer):
     # Create layer
     layer_name = f"{layer.name()}: Invalid cellsize"
     errors_layer = QgsVectorLayer("Polygon", layer_name, "memory")
@@ -34,7 +34,7 @@ def validate_cellsize(layer):
     return errors_layer
 
 
-def validate_intersect(layers_dict) -> QgsVectorLayer:
+def validate_intersect(layers_dict: dict) -> QgsVectorLayer:
     # Combine ground and roofs layers
     data = pd.concat(map(layer_to_gdf, layers_dict.values()))
 
@@ -69,7 +69,7 @@ def get_multipolygon_vertices(geom: shapely.MultiPolygon) -> list:
         verts += get_polygon_vertices(poly)
     return verts
 
-def validate_vert_edge(layer_input) -> QgsVectorLayer:
+def validate_vert_edge(layer_input: QgsVectorLayer) -> QgsVectorLayer:
     data: gpd.GeoDataFrame = layer_to_gdf(layer_input)
 
     error_layer = QgsVectorLayer("Point", "Vertex-Edge Errors", "memory")
@@ -109,8 +109,45 @@ def validate_vert_edge(layer_input) -> QgsVectorLayer:
     return error_layer
 
 
-def validate_input_layers(layers_dict):
+def validate_roof_layer(layer: QgsVectorLayer):
+    # Create layer
+    errors_layer = QgsVectorLayer("Polygon", "Invalid Roof Params", "memory")
+    errors_layer.setCrs(layer.crs())
+    provider = errors_layer.dataProvider()
+
+    print(layer.attributeList())
+
+    fid_field = QgsField("fid", QVariant.Int)
+    roughness_field = QgsField("roughness", QVariant.Double)
+    elev_field = QgsField("elev", QVariant.Double)
+    provider.addAttributes([fid_field, roughness_field, elev_field])
+    errors_layer.updateFields()
+
+    # Fill layer with cellsize errors
+    errors_layer.startEditing()
+    for feature in layer.getFeatures():
+        roughness = feature["roughness"]
+        elev = feature["elev"]
+        if type(roughness) in [int, float] and roughness > 0:
+            continue
+        if type(elev) in [int, float] and elev > 0:
+            continue
+        invalid_feature = QgsFeature(errors_layer.fields())
+        invalid_feature.setAttributes([feature["fid"], feature["roughness"], feature["elev"]])
+        invalid_feature.setGeometry(feature.geometry())
+        errors_layer.addFeature(invalid_feature)
+    errors_layer.commitChanges()
+
+    return errors_layer
+
+
+def validate_input_layers(layers_dict: dict) -> list:
     error_layers = []
+
+    # Validate ground layer
+    roof_attrs_layer = validate_roof_layer(layers_dict["roof"])
+    if roof_attrs_layer.hasFeatures():
+        error_layers.append(roof_attrs_layer)
 
     # Validate vert-edge
     vert_edge_layer = validate_vert_edge(layers_dict["ground"])
