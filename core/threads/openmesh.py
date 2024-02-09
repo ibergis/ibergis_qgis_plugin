eaaf5e78263450d016652eb6d5ec8c7748a14733
nullptr <eliesbertran@gmail.com>
2024-02-09 19:07:40 +0100

    Removed old code

--- core/threads/openmesh.py
+++ core/threads/openmesh.py
@@ -41,54 +41,6 @@ class DrOpenMeshTask(DrTask):
         self.layer = temp_layer
         return True
 
-        temp_layer = QgsVectorLayer("Polygon", "Mesh Temp Layer", "memory")
-        temp_layer.setCrs(QgsProject.instance().crs())
-        provider = temp_layer.dataProvider()
-        fields = [
-            QgsField("fid", QVariant.Int),
-            QgsField("category", QVariant.String),
-            QgsField("vertex_id1", QVariant.Int),
-            QgsField("vertex_id2", QVariant.Int),
-            QgsField("vertex_id3", QVariant.Int),
-            QgsField("vertex_id4", QVariant.Int),
-            QgsField("roughness", QVariant.Double),
-        ]
-        provider.addAttributes(fields)
-        temp_layer.updateFields()
-
-        total_tri = len(mesh["polygons"])
-        counter_tri = 0
-        for i, tri in mesh["polygons"].items():
-            feature = QgsFeature()
-            vertices = (mesh["vertices"][vert] for vert in tri["vertice_ids"])
-            wkt = "POLYGON(("
-            wkt += ",".join(
-                f"{v['coordinates'][0]} {v['coordinates'][1]} {v['elevation']}"
-                for v in vertices
-            )
-            wkt += "))"
-            feature.setGeometry(QgsGeometry.fromWkt(wkt))
-            feature.setAttributes(
-                [i, tri["category"], *tri["vertice_ids"], tri["roughness"]]
-            )
-            provider.addFeature(feature)
-            counter_tri += 1
-            self.update_polygon_progress(counter_tri / total_tri)
-
-        temp_layer.updateExtents()
-
-        self.setProgress(self.POST_LAYER_PROGRESS)
-
-        # Set the style of the layer
-        style_path = "resources/templates/mesh_temp_layer.qml"
-        style_path = Path(global_vars.plugin_dir) / style_path
-        style_path = str(style_path)
-        temp_layer.loadNamedStyle(style_path)
-        temp_layer.triggerRepaint()
-
-        self.layer = temp_layer
-        return True
-
     def update_polygon_progress(self, tri_progress: float):
         progress = (
             self.POST_LAYER_PROGRESS - self.POST_FILE_PROGRESS
