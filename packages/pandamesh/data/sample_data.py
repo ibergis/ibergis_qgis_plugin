522c25f6b27fab9875873d3f5e43a3fe385ca7ab
smaspons <smaspons@bgeo.es>
2025-04-24 13:03:55 +0200

    feat(packages): add triangle package and don't use pooch

--- packages/pandamesh/data/sample_data.py
+++ packages/pandamesh/data/sample_data.py
@@ -2,18 +2,18 @@
 Functions to load sample data.
 """
 import geopandas as gpd
-import pooch
+# import pooch
 
-REGISTRY = pooch.create(
-    path=pooch.os_cache("pandamesh"),
-    base_url="https://github.com/deltares/pandamesh/raw/main/data/",
-    version=None,
-    version_dev="main",
-    env="PANDAMESH_DATA_DIR",
-    registry={
-        "provinces-nl.geojson": "7539318974d1d78f35e4c2987287aa81f5ff505f444a2e0f340d804f57c0f8e3",
-    },
-)
+# REGISTRY = pooch.create(
+#     path=pooch.os_cache("pandamesh"),
+#     base_url="https://github.com/deltares/pandamesh/raw/main/data/",
+#     version=None,
+#     version_dev="main",
+#     env="PANDAMESH_DATA_DIR",
+#     registry={
+#         "provinces-nl.geojson": "7539318974d1d78f35e4c2987287aa81f5ff505f444a2e0f340d804f57c0f8e3",
+#     },
+# )
 
 
 def provinces_nl():
