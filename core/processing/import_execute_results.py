a4d29c30416d8bee68356f8fff50146a63df04c4
opueyo <opueyo@bgeo.es>
2025-04-24 14:05:24 +0200

    feat(import_execute_results): create style, config temporal and open temporal controller

--- core/processing/import_execute_results.py
+++ core/processing/import_execute_results.py
@@ -17,13 +17,22 @@ from qgis.core import (
     QgsProcessingContext,
     QgsRasterLayerTemporalProperties,
     Qgis,
-    QgsDateTimeRange
+    QgsDateTimeRange,
+    QgsColorRampShader,
+    QgsRasterShader,
+    QgsSingleBandPseudoColorRenderer,
+    QgsInterval,
+    QgsTemporalNavigationObject,
+    QgsStyle,
+    QgsGradientColorRamp
 )
-from qgis.PyQt.QtCore import QCoreApplication, QDateTime
+from qgis.PyQt.QtCore import QCoreApplication, QDateTime, QTimeZone
+from qgis.PyQt.QtGui import QColor, QAction
 from ...core.utils import Feedback
 from typing import Optional
 from datetime import datetime
 from swmm_api import read_inp_file
+from ... import global_vars
 import os
 
 
@@ -44,6 +53,8 @@ class ImportExecuteResults(QgsProcessingAlgorithm):
     end_date: Optional[datetime] = None
     end_time: Optional[datetime] = None
     step_time: Optional[datetime] = None
+
+    iface = global_vars.iface
 
 
 
@@ -71,6 +82,8 @@ class ImportExecuteResults(QgsProcessingAlgorithm):
         """
         main process algorithm of this tool
         """
+
+        self.iface = global_vars.iface
 
         # reading geodata
         feedback.setProgressText(self.tr('Reading folder...'))
@@ -121,7 +134,9 @@ class ImportExecuteResults(QgsProcessingAlgorithm):
 
         # Create layer group
         if group_name == "" or group_name is None:
-            group_name: str = "Results - " + datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+            group_name: str = "RESULTS - " + datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+        else:
+            group_name = f"RESULTS - {group_name}"
         root: QgsLayerTreeLayer = project.layerTreeRoot()
         self.layers_group = root.addGroup(group_name)
 
@@ -142,32 +157,52 @@ class ImportExecuteResults(QgsProcessingAlgorithm):
 
     def postProcessAlgorithm(self, context: QgsProcessingContext, feedback: Feedback):
         # Add layers
+        first_layer: QgsRasterLayer = None
         for layer in self.layers:
+            if first_layer is None:
+                first_layer = layer
             QgsProject.instance().addMapLayer(layer, False)
             self.configLayer(layer, feedback)
             feedback.setProgressText(self.tr(f'Layer {layer.name()} added correctly.'))
             if self.layers_group is not None:
                 self.layers_group.addLayer(layer)
         feedback.setProgressText(self.tr(f"Importing process finished."))
+        feedback.setProgressText(self.tr(f'Configuring temporal line...'))
+        self.openTemporalLine(first_layer)
         feedback.setProgress(100)
 
         return {}
 
     def configLayer(self, layer: QgsRasterLayer, feedback: Feedback):
-        """ Method to configure the raster layer. Setting its style and temporal configuration """
-        # Load style
-        if os.path.exists(f'C:{os.sep}Users{os.sep}Usuario{os.sep}Documents{os.sep}_DRAIN{os.sep}blues_2.qml'): #TODO set plugin path when the styles have made
-            layer.loadNamedStyle(f'C:{os.sep}Users{os.sep}Usuario{os.sep}Documents{os.sep}_DRAIN{os.sep}blues_2.qml')
-            feedback.setProgressText(self.tr(f"Style QML found."))
-        else:
-            feedback.reportError(self.tr(f'Style QML not found.'))
+        """ Configure style and temporal configuration to layer """
+
+        # Set main colors
+        main_colors: dict = {0: QColor(0,0,255), 0.5: QColor(0,255,255), 1: QColor(0,255,0), 1.5: QColor(255,255,0), 2: QColor(255,0,0)}
+
+        # Create color ramp
+        color_items = self.createColorRamp(main_colors, 0, 2)
+
+        # Create shader
+        shader = QgsRasterShader()
+        color_ramp = QgsColorRampShader(0,2)
+        color_ramp.setColorRampItemList(color_items)
+        color_ramp.setColorRampType(QgsColorRampShader.Interpolated)
+        shader.setRasterShaderFunction(color_ramp)
+
+        # Create render
+        provider = layer.dataProvider()
+        renderer = QgsSingleBandPseudoColorRenderer(provider, 0, shader)
+
+        # Apply render
+        layer.setRenderer(renderer)
+        layer.triggerRepaint()
 
         # Config temporal
         temporal: QgsRasterLayerTemporalProperties = layer.temporalProperties()
         temporal.setIsActive(True)
         temporal.setMode(Qgis.RasterTemporalMode.FixedRangePerBand)
 
-        start_time: QDateTime = QDateTime(self.start_date, self.start_time)
+        start_time: QDateTime = QDateTime(self.start_date, self.start_time, QTimeZone(b"Europe/Berlin"))
         interval_seconds: Optional[int] = None
         if self.step_time is not None:
             interval_seconds = (self.step_time.hour * 60 + self.step_time.minute) * 60 + self.step_time.second
@@ -181,6 +216,90 @@ class ImportExecuteResults(QgsProcessingAlgorithm):
             start_time = start_time.addSecs(interval_seconds)
 
         temporal.setFixedRangePerBand(ranges)
+
+
+    def createColorRamp(self, main_colors: dict, min_val: float, max_val: float, num_steps: int = 20, min_invisible: bool = True):
+        """ Creates a color ramp from main colors
+                Parameters:
+                    main_colors --> Main color references to make the gradient
+                    min_val --> Minimum value
+                    max_val --> Maximum value
+                    num_steps --> Number of ramp values. The more values, more precise color
+                    min_invisible --> Boolean to set the minimum value(-9999) invisible
+        """
+
+        # Calculate value difference
+        step: float = (max_val - min_val) / (num_steps - 1)
+        # Create value list
+        values: list[float] = [min_val + i * step for i in range(num_steps)]
+
+        # Calculate each value color
+        color_items: list[QgsColorRampShader.ColorRampItem] = []
+        for val in values:
+            # Get parent main colors
+            keys: list[float] = sorted(main_colors.keys())
+            for i in range(len(keys)-1):
+                if keys[i] <= val <= keys[i+1]:
+                    ratio = (val - keys[i]) / (keys[i+1] - keys[i])
+                    c1 = main_colors[keys[i]]
+                    c2 = main_colors[keys[i+1]]
+
+                    # Calculate color
+                    r = int(c1.red() + (c2.red() - c1.red()) * ratio)
+                    g = int(c1.green() + (c2.green() - c1.green()) * ratio)
+                    b = int(c1.blue() + (c2.blue() - c1.blue()) * ratio)
+
+                    color = QColor(r, g, b)
+                    break
+            else:
+                color = QColor(0, 0, 0)  # Default color
+            # Store color into color list
+            color_items.append(QgsColorRampShader.ColorRampItem(val, color, f"{val}".replace('.', ',')))
+        if min_invisible:
+            # Set minimum value(-9999) invisible
+            color_items.insert(0, QgsColorRampShader.ColorRampItem(-9999.0, QColor(0,0,0,0), "-9999.0"))
+
+        return color_items
+
+
+    def openTemporalLine(self, layer: QgsRasterLayer):
+        """ Configure and open the QGIS temporal line """
+
+        # Get the temporal controller from the QGIS project
+        temporal_controller = self.iface.mapCanvas().temporalController()
+        if not temporal_controller:
+            return
+
+        # Check if the layer has active temporal properties
+        temporal_properties = layer.temporalProperties()
+        if not temporal_properties.isActive():
+            return
+
+        # Close the temporal controller if it's open
+        action = self.iface.mainWindow().findChild(QAction, 'mActionTemporalController')
+        if action.isChecked():
+            action.trigger()
+
+        if None in (self.start_date, self.end_date):
+            return
+        min_time: QDateTime = QDateTime(self.start_date, self.start_time)
+        max_time: QDateTime = QDateTime(self.end_date, self.end_time)
+        temporal_controller.setTemporalExtents(QgsDateTimeRange(min_time, max_time))
+
+        # Set frame duration using QgsInterval for 1 second
+        if self.step_time is None:
+            return
+        frame_duration = QgsInterval((self.step_time.hour * 60 + self.step_time.minute) * 60 + self.step_time.second)
+        temporal_controller.setFrameDuration(frame_duration)
+        temporal_controller.setFramesPerSecond(1.0)
+
+        # Set navigation mode to Animated for dynamic playback
+        temporal_controller.setNavigationMode(QgsTemporalNavigationObject.NavigationMode.Animated)
+
+        # Open temporal controller
+        action = self.iface.mainWindow().findChild(QAction, 'mActionTemporalController')
+        action.trigger()
+
 
     def shortHelpString(self):
         return self.tr("""WIP Process""")
