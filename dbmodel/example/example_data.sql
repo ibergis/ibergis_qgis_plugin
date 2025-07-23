0b8ab2bd6cdca6e33bbc6d44d6b86116b49f685c
opueyo <opueyo@bgeo.es>
2025-07-23 17:29:25 +0200

    fix(example_data): change inlet method from UPC to W_O

--- dbmodel/example/example_data.sql
+++ dbmodel/example/example_data.sql
@@ -675,8 +675,8 @@ UPDATE roof SET isconnected=1;
 
 -- inlet
 UPDATE inlet SET outlet_type = 'TO NETWORK', method = 'W_O', weir_cd = 1.6 , orifice_cd = 0.7, efficiency = 90 , width = 1, length = 0.2 where fid < 99;
-UPDATE inlet SET outlet_type = 'TO NETWORK', method = 'UPC', a_param = 0.496 , b_param = 0.712, efficiency = 90 , width = 1, length = 0.2 where fid > 98;
-UPDATE inlet SET outlet_type = 'SINK', method = 'UPC', a_param = 0.496 , b_param = 0.712, efficiency = 90 , width = 1, length = 0.2 where fid = 44;
+UPDATE inlet SET outlet_type = 'TO NETWORK', method = 'W_O', a_param = 0.496 , b_param = 0.712, efficiency = 90 , width = 1, length = 0.2 where fid > 98;
+UPDATE inlet SET outlet_type = 'SINK', method = 'W_O', a_param = 0.496 , b_param = 0.712, efficiency = 90 , width = 1, length = 0.2 where fid = 44;
 UPDATE inlet SET code = 1000+fid;
 
 -- ground
