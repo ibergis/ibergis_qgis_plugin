b3c26213b758cfc58c7affc04191fe3856597743
smaspons <smaspons@bgeo.es>
2025-07-30 12:14:37 +0200

    fix(flake8): add missing import in toolbox_btn

--- core/toolbars/utilities/toolbox_btn.py
+++ core/toolbars/utilities/toolbox_btn.py
@@ -15,7 +15,7 @@ from datetime import timedelta
 from qgis.PyQt.QtCore import Qt, QTimer
 from qgis.PyQt.QtGui import QIcon, QStandardItemModel, QStandardItem
 from qgis.PyQt.QtWidgets import QSpinBox, QWidget, QLineEdit, QComboBox, QCheckBox, QRadioButton, QAbstractItemView, \
-    QCompleter, QGridLayout, QLabel, QTableWidgetItem, QFileDialog
+    QCompleter, QGridLayout, QLabel, QTableWidgetItem, QFileDialog, QHeaderView
 from qgis.core import QgsApplication, QgsProject
 from qgis.gui import QgsDateTimeEdit
 
